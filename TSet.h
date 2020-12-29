31d0e112d9e5d5a203ea0135384e26b81b9c7eef
Ilya Leb <iluxa00701@gmail.com>
2020-12-29 19:15:06 +0300

    Add realization of class Tset and TBitField

---
+++ TSet.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "TBitField.h"
+class TSet
+{
+private:
+	int MaxPower;//максимальный мощность множества 
+	TBitField BitField;
+public:
+	TSet(int maxPower);
+	TSet(const TSet& set);//конструктор копирования
+	TSet(TBitField& bf);//конструктор преобразования 
+	//Доступ к битам
+	int GetMaxPower()const;//максимальное мощноесть множества
+	void InsElem(const int n);//включить элемнт в множество 
+	void DelElem(const int n);//удалить элемнеть в множество
+	bool IsMember(const int n);//проверить наличие элементов в множестве
+	//операции
+	bool operator==(const TSet& set)const;//сравнение 
+	TSet& operator=(const TSet& set);//Присваивание
+	TSet operator+(const TSet& set)const;//Объединение
+	TSet operator*(const TSet& set)const;//Пересечение 
+	TSet operator+(const int n);//Включение эл-та в мн-ва
+	TSet operator-(const int n);//Исключение эл-та из мн-ва
+	TSet operator~();//инверсия
+	friend istream& operator >>(istream&ist, TSet& bf);//опепатор ввода
+	friend ostream& operator <<(ostream&ost, TSet& bf);//оператор вывода
+};
